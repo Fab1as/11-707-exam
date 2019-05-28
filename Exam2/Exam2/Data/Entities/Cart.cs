3fd4340eb6cc761fdb024d53b9c34d6102e094fc
Fab1as <fabiasser@gmail.com>
2019-05-28 14:09:52 +0300

    Added exam2

---
+++ Exam2/Exam2/Data/Entities/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam2.Data.Entities
+{
+    public class Cart : BaseEntity
+    {
+        public IEnumerable<Dish> Dishes { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
